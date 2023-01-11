0a1a790f909b46ea3a441ffeb18ed0a6e1323f51
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2023-01-11 12:45:13 +0200

    adds changes

--- src/js/dom/renderTrendingFilms.js
+++ src/js/dom/renderTrendingFilms.js
@@ -1,28 +1,9 @@
 import { getTrendingFilms } from '../api/fetchAPI';
+import { renderGallery } from './renderMovies';
 
-const moviesOnInputList = document.querySelector('.list-gallery');
+renderTrendingFilms();
 
-function renderTrendingGallery(data) {
-  getTrendingFilms();
-
-  const markupGallery = data
-    .map(
-      ({ original_title, poster_path, id, genre_names, release_date }) =>
-        `<li class="movie__item" data-id="${id}">
-                <div class="movie__img">
-                <img src= "https://image.tmdb.org/t/p/original${poster_path}" alt="${original_title}" loading="lazy">
-        </div>
-                <div class="movie__description">
-                  <p class="movie__title">"${original_title}"</p>
-                  <div class="movie__meta">
-                    <p class="movie__genres">"${genre_names}"</p>
-                    <p class="movie__data">"${release_date}"</p>
-                  </div>
-                </div>
-            </li>`
-    )
-    .join('');
-  moviesOnInputList.insertAdjacentHTML('beforeend', markupGallery);
+async function renderTrendingFilms() {
+  const data = await getTrendingFilms;
+  renderGallery(data.results);
 }
-
-console.log(renderTrendingGallery());
