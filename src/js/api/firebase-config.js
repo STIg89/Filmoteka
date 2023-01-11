71e9342f5c6280773fd48e93a610dba996ffb4cc
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2023-01-11 10:12:17 +0200

    Adds firebase related components
     - Adds firebase-config for initialisation
     - Adds sign-up, sign-in, sign-out functions

---
+++ src/js/api/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDv5dHMjgq_6GWD35Of-cTXskRk1rXncws',
+  authDomain: 'filmoteka-command-project-js.firebaseapp.com',
+  projectId: 'filmoteka-command-project-js',
+  storageBucket: 'filmoteka-command-project-js.appspot.com',
+  messagingSenderId: '1084933608284',
+  appId: '1:1084933608284:web:16097211c819e6fa8d6799',
+  measurementId: 'G-BHYJP9WVT4',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
