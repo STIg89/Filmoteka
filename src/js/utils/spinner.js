ebf09996da2068ae2797395d089484a2b3e87389
zevgen83 <108744200+zevgen83@users.noreply.github.com>
2023-01-12 23:12:32 +0200

    spinner

---
+++ src/js/utils/spinner.js
@@ -0,0 +1,16 @@
+import {pagePreloader} from '../dom/refs';
+
+function showSpinner() {
+  pagePreloader.classList.remove('hide');
+}
+function hideSpinner() {
+  pagePreloader.classList.add('hide');
+}
+
+export { showSpinner, hideSpinner };
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    pagePreloader.classList.add('hide');
+  }, 1000);
+});
