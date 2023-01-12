45dd4d7f3cbd40d3e77c209133a2856ddceb39b9
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2023-01-12 14:45:19 +0200

    Modal signup
    Adds error message to signup if already registered

--- src/js/dom/modalSignUp.js
+++ src/js/dom/modalSignUp.js
@@ -1,20 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from '../api/firebase-config.js';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-// import { showLoginError } from './showLoginError.js';
+import { showSignUpError } from './showErrors.js';
 const signUpLoginRef = document.getElementById('signup-user-login');
 const signUpPasswordRef = document.getElementById('signup-user-password');
 const signUpFormRef = document.getElementById('signup-form');
 const signUpBtnRef = document.querySelector('.signup-modal__button');
+const policyCheckboxRef = document.querySelector('[name="policy-checkbox"]');
 
 console.log(signUpFormRef);
 async function createAccount(event) {
   event.preventDefault();
-
   const email = event.currentTarget.elements.useremail.value;
-  console.log(email);
   const password = event.currentTarget.elements.useremail.value;
-  console.log(password);
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   try {
@@ -24,10 +22,20 @@ async function createAccount(event) {
       password
     );
     const user = userCredential.user;
+    showSignUpSuccess(user); // no function created
   } catch (error) {
-    console.log(error);
-    showLoginError(error);
+    showSignUpError(error);
   }
 }
 
 signUpFormRef.addEventListener('submit', createAccount);
+
+policyCheckboxRef.addEventListener('click', toggleBtnProperty);
+
+function toggleBtnProperty(evt) {
+  if (evt.target.checked) {
+    signUpBtnRef.disabled = false;
+  } else {
+    signUpBtnRef.disabled = true;
+  }
+}
