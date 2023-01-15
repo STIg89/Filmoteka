05cb3df8385c14461883de6e6d2ee4df545ca960
STIg89 <andreystig89@gmail.com>
2023-01-15 12:05:38 +0200

    queueLS update

--- src/js/dom/queueLS.js
+++ src/js/dom/queueLS.js
@@ -1,15 +1,27 @@
 let queueAddBtn = document.querySelector('.add-queue-btn');
 let queueBtn = document.querySelector('.queue-btn');
-let moviesQueue = [];
+let key = 'moviesQueue';
 
-queueAddBtn.addEventListener('click', OnAddQueueClick);
+queueAddBtn.addEventListener('click', onAddQueueClick);
+// queueBtn.addEventListener('click', getQueue);
 
-async function getQueue() {
-
+async function onAddQueueClick(event) {
+  event.preventDefault();
+  queueAddBtn.disabled = true;
 }
 
-async function OnAddQueueClick(event) {
-    event.preventDefault();
-    queueAddBtn.disabled = true;
+async function addToQueue(movieId) {
+  let array = await getQueue();
 
+  if (!array.includes(movieId)) {
+    array.push(movieId);
+    localStorage.setItem(key, JSON.stringify(array));
+  }
+}
+
+async function getQueue() {
+  let movieArray = localStorage.getItem(key);
+  let parsedMovieArray = JSON.parse(movieArray);
+
+  return Array.isArray(parsedMovieArray) ? parsedMovieArray : [];
 }
