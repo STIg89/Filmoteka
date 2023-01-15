8fd43ff55a165b6412952b64fc8ee663030198a0
SerhyRymar <serhyrymar@gmail.com>
2023-01-15 14:40:22 +0200

    add_swiper

--- src/js/dom/footerModal.js
+++ src/js/dom/footerModal.js
@@ -1,3 +1,23 @@
+const { default: Swiper } = require("swiper");
+
+new Swiper('.mySwiper', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
+
+
 const modalRefs = document.querySelectorAll('#footer-modal-open');
 const modals = document.querySelectorAll('.modal');
 const body = document.body;
