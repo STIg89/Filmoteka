d1ba432228380a363bede5e9912fb798dcbe15b5
STIg89 <andreystig89@gmail.com>
2023-01-15 14:27:58 +0200

    library.js for library.html

--- src/js/dom/dom.js
+++ src/js/dom/dom.js
@@ -8,7 +8,6 @@ import './modalSignUp';
 import './renderMovieModal';
 import './renderMovies';
 import './renderTrendingFilms';
-import './movieModal';
 import './trailer';
 import './watchedLS';
 import './queueLS';
---
+++ src/library.js
@@ -0,0 +1,15 @@
+import './js/api/fetchAPI';
+
+import './js/dom/footerModal';
+import './js/dom/movieModal';
+import './js/dom/renderMovieModal';
+import './js/dom/renderMovies';
+import './js/dom/trailer';
+import './js/dom/watchedLS';
+import './js/dom/queueLS';
+
+import './js/utils/pagination';
+import './js/utils/scrollToTop';
+import './js/utils/spinner';
+import './js/utils/themes';
+// import './js/utils/modalsOpenClose';
