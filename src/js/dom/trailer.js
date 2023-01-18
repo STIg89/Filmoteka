5d122c3823fca4c0b6506a5b2165929f49fda684
STIg89 <andreystig89@gmail.com>
2023-01-18 14:59:27 +0200

    Update trailer.js

--- src/js/dom/trailer.js
+++ src/js/dom/trailer.js
@@ -1,5 +1,4 @@
 import * as basicLightbox from 'basiclightbox';
-// import 'basiclightbox/dist/basiclightbox.min.css';
 import { getMovieTrailer } from '../api/fetchAPI';
 import { refs } from './refs';
 
