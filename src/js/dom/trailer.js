e441037aa67b87e0021342b5af1056a6238c3f8d
STIg89 <andreystig89@gmail.com>
2023-01-18 02:28:48 +0200

    Update trailer.js

--- src/js/dom/trailer.js
+++ src/js/dom/trailer.js
@@ -1,33 +1,30 @@
 import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basiclightbox.min.css';
 import { getMovieTrailer } from '../api/fetchAPI';
 import { refs } from './refs';
 
-
 function addListenerAddBtnTrailer() {
-    const trailer = document.querySelector('#trailer');
-    trailer.addEventListener('click', onClickBtnTrailer);
+  const trailer = document.querySelector('#trailer');
+  trailer.addEventListener('click', onClickBtnTrailer);
 }
 
 async function onClickBtnTrailer(e) {
+  let movieID = Number(refs.modalMovieContent.getAttribute('data-id'));
 
-    let movieID = Number(refs.modalMovieContent.getAttribute('data-id'));
-    
-    const data = await getMovieTrailer(movieID);    
-    const { results } = data;
-   
-    let key = '';
-    results.forEach(data => {
-        if (data.type === 'Trailer') {
-            key = data.key;
-        };        
-    });
+  const data = await getMovieTrailer(movieID);
+  const { results } = data;
 
-    const instance = basicLightbox.create(`  
+  let key = '';
+  results.forEach(data => {
+    if (data.type === 'Trailer') {
+      key = data.key;
+    }
+  });
+
+  const instance = basicLightbox.create(`  
   <iframe  width="660" height="415" src='https://www.youtube.com/embed/${key}' frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 `);
-  instance.show();  
+  instance.show();
 }
 
-export {
-    addListenerAddBtnTrailer,
-};
+export { addListenerAddBtnTrailer };
